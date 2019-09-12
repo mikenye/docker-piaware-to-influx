1a7a89ebcafa33a217504054855b0404d874e78f
mikenye <mike.nye@xpanse.com.au>
2019-09-12 20:52:46 +0800

    Add arm64 dockerfile (aarch64)

--- build_aarch64.sh
+++ build_aarch64.sh
@@ -3,4 +3,4 @@
 VERSION="2019-09-12"
 IMAGE=mikenye/piaware-to-influx
 
-docker build -f Dockerfile -t ${IMAGE}:${VERSION}-arm64v8 .
+docker build -f Dockerfile.aarch64 -t ${IMAGE}:${VERSION}-arm64v8 .
