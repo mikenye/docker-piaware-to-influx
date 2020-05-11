46075678ad2f0a589740872d89e7eaa2181fc9e6
Mike Nye <mike.nye@gmail.com>
2020-05-11 09:35:31 +0800

    Add armv6 architecture support & update build script

--- buildx.sh
+++ buildx.sh
@@ -3,39 +3,18 @@
 
 REPO=mikenye
 IMAGE=piaware-to-influx
+PLATFORMS="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
 
-# build temp image to get versions
-docker build -t "${REPO}/${IMAGE}:temp" .
-docker run --rm --entrypoint cat "${REPO}/${IMAGE}:temp" /VERSIONS > "/tmp/${REPO}_${IMAGE}.current"
-docker run --rm --entrypoint cat "${REPO}/${IMAGE}:latest" /VERSIONS > "/tmp/${REPO}_${IMAGE}.latest"
+# Build the latest image using buildx
+docker buildx build -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
 
-# Check for version changes between this build and :latest
-echo ""
-echo "Version changes:"
-echo ""
-diff "/tmp/${REPO}_${IMAGE}.latest" "/tmp/${REPO}_${IMAGE}.current"
-# DIFFEXITCODE=$?
-echo ""
+# Get version of the latest image
+docker pull "${REPO}/${IMAGE}:latest"
+VERSION=$(docker run --rm --entrypoint /piaware2influx.py ${REPO}/${IMAGE}:latest --version | cut -d " " -f 2)
 
-# If versions have changed from latest image, then we rebuild latest
-# if [ "$DIFFEXITCODE" -ne "0" ]; then
-
-# Get version
-VERSION=$(cat "/tmp/${REPO}_${IMAGE}.current" | grep piaware2influx.py | cut -d " " -f 2)
-
-# Build the image using buildx
-docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
-docker buildx build -t "${REPO}/${IMAGE}:latest" --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
-
-# else
-#   echo "No version changes, nothing further to do."
-#   echo ""
-#
-# fi
-
-# Clean up
-rm "/tmp/${REPO}_${IMAGE}.current" "/tmp/${REPO}_${IMAGE}.latest"
+# Build version-specific tagged image
+docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platform "${PLATFORMS}" .
